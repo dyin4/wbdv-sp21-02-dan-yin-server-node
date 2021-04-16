193f2fcd8a727d5abecab14563fe23fd6464200a
Dan Yin <danyin@Dans-MacBook-Pro.local>
2021-04-15 18:45:25 -0700

    add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose');
+console.log("Database_URL", process.env.MONGO_DB_URI);
 const uri = process.env.MONGO_DB_URI || 'mongodb+srv://dyin4:yindan77@cluster0.zyayu.mongodb.net/whiteboard-02'
 mongoose.connect(uri,
     {useNewUrlParser: true, useUnifiedTopology: true});
@@ -19,6 +20,12 @@ app.use(function (req, res, next) {
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.get('/', function(req, res) {
+
+  // ejs render automatically looks in the views folder
+  res.render("<h1>Hello World!</h1>");
+});
+
 
 
 require('./controllers/quizzes-controller')(app)
